19b150cec46a03bda0056752144abaaee2700312
abricker <abricker@wustl.edu>
2018-11-04 10:25:58 -0600

    Added registration page, advance search viewer

--- js/search_page.js
+++ js/search_page.js
@@ -1,1 +1,11 @@
 // Code for manipulating the search page
+
+function toggleAdvanced() {
+    var adv = document.getElementById("advanced")
+
+    if (adv.style.visibility == "hidden") {
+        adv.style.visibility = "visible"
+    } else {
+        adv.style.visibility = "hidden"
+    }
+}
