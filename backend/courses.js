ef36a8f209206ad6525edd549081897b3790edc6
abricker <abricker@wustl.edu>
2018-11-05 09:28:36 -0600

    Added basic search functionality

--- backend/courses.js
+++ backend/courses.js
@@ -2,15 +2,26 @@
 // This way we skip the extra step of having to read/write to a file, and we're allowed to 'fake it' anyway, so whatever
 
 var classesDB = [
-//   Department, Course #, Start time, End time, Weekdays, Professor,     Name
-    ["CSE",      131,      1130,       1300,     "TR",     "Cytron",      "Computer Science I"],
-    ["CSE",      132,      1130,       1300,     "MW",     "Siever",      "Computer Science II"],
-    ["CSE",      204,      830,        1000,     "TR",     "Hufstedler",  "Web Development"],
-    ["CSE",      217,      1600,       1730,     "R",      "Neumann",     "Intro to Data Science"],
-    ["CSE",      231,      1000,       1130,     "TR",     "Cosgrove",    "Intro to Parallel and Concurrent Programming"],
-    ["CSE",      240,      830,        1000,     "MW",     "Sproull",     "Logic and Discrete Math"],
-    ["CSE",      247,      1430,       1600,     "T",      "Cole",        "Data Structures and Algorithms"],
-    ["CSE",      260,      1300,       1430,     "MW",     "Richard",     "Intro to Digital Logic"],
-    ["CSE",      330,      1000,       1130,     "MW",     "Sproull",     "Rapid Prototype Development"],
-    ["CSE",      332,      1000,       1130,     "MW",     "Shidal",      "Object-Oriented Software Development"],
+//   School, Department, Course #, Start time, End time, Weekdays, Professor,     Name
+    ["ENG",  "CSE",      131,      1130,       1300,     "TR",     "Cytron",      "Computer Science I"],
+    ["ENG",  "CSE",      132,      1130,       1300,     "MW",     "Siever",      "Computer Science II"],
+    ["ENG",  "CSE",      204,      830,        1000,     "TR",     "Hufstedler",  "Web Development"],
+    ["ENG",  "CSE",      217,      1600,       1730,     "R",      "Neumann",     "Intro to Data Science"],
+    ["ENG",  "CSE",      231,      1000,       1130,     "TR",     "Cosgrove",    "Intro to Parallel and Concurrent Programming"],
+    ["ENG",  "CSE",      240,      830,        1000,     "MW",     "Sproull",     "Logic and Discrete Math"],
+    ["ENG",  "CSE",      247,      1430,       1600,     "T",      "Cole",        "Data Structures and Algorithms"],
+    ["ENG",  "CSE",      260,      1300,       1430,     "MW",     "Richard",     "Intro to Digital Logic"],
+    ["ENG",  "CSE",      330,      1000,       1130,     "MW",     "Sproull",     "Rapid Prototype Development"],
+    ["ENG",  "CSE",      332,      1000,       1130,     "MW",     "Shidal",      "Object-Oriented Software Development"],
 ]
+
+// A map of Schools to an array of its departments
+var deptMap = {
+    "ARCH" : [],
+    "ARTSCI" : [],
+    "BUS" : [],
+    "DVA" : [],
+    "ENG" : ["CSE"],
+    "LAW" : [],
+    "MED" : []
+}
--- js/search_page.js
+++ js/search_page.js
@@ -13,16 +13,63 @@ function toggleAdvanced() {
 function showResults(matches) {
     // matches is an array of courses to display
     var results = document.getElementById("searchResults")
+    // Delete previous children
+    while (results.firstChild) {
+        results.removeChild(results.firstChild);
+    }
 
     for (var i = 0; i < matches.length; i++) {
         var res = document.createElement("div")
+        // TODO: Make this a nice and pretty div
         var name = document.createElement("p")
-        name.innerHTML = matches[i][0] + " " + matches[i][1] + " - " + matches[i][6]
+        name.innerHTML = matches[i][1] + " " + matches[i][2] + " - " + matches[i][7]
         res.appendChild(name)
 
         results.appendChild(res)
     }
 }
 
-// This shouldn't be called until search is input, only here for testing
-showResults(classesDB)
+function search() {
+    var matches = []
+    var school = document.getElementById("washuSchool").value
+    var department = document.getElementById("department").value
+    // var semester = document.getElementById("semester").value
+
+    for (var i = 0; i < classesDB.length; i++) {
+        // Course matches school search
+        if ((classesDB[i][0] == school) || (school == "ALL")) {
+            if ((classesDB[i][1] == department) || (department == "ALL")) {
+                matches.push(classesDB[i])
+            }
+        }
+    }
+
+    showResults(matches)
+}
+
+// When the "school" dropdown is changed, the options in the "department" dropdown should change
+function populateDepartment() {
+    var school = document.getElementById("washuSchool").value
+    var departmentNode = document.getElementById("department")
+    // Clear previous dropdown
+    while (departmentNode.firstChild) {
+        departmentNode.removeChild(departmentNode.firstChild);
+    }
+    // Always have the "All departments" option
+    var opt = document.createElement("option")
+    opt.value = "ALL"
+    opt.innerHTML = "All Departments"
+    departmentNode.append(opt)
+
+    if (school == "ALL") {
+        // Populate with all keys in map
+    } else {
+        var dpts = deptMap[school]
+        for (var i = 0; i < dpts.length; i++) {
+            var opt = document.createElement("option")
+            opt.value = dpts[i]
+            opt.innerHTML = dpts[i]
+            departmentNode.append(opt)
+        }
+    }
+}
